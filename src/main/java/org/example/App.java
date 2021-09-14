88abc418e736ed4b3416ae0a3ae9706b80421eaa
Pedro Henrique Sotto-Mayor Pereira da Silva <itapeu16@hotmail.com>
2021-09-13 23:25:22 -0700

    Added comment to the top of the document

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,11 +1,11 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Pedro Henrique Sotto-Mayor Pereira da Silva
+ */
 package org.example;
 
 import java.util.Scanner;
 
-/**
- * Hello world!
- *
- */
 public class App
 {
     public static void main( String[] args )
